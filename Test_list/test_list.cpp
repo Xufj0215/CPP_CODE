51e2d5697d71d9a3525a4b4781d86f7c3f2beb9b
xushujie <Xufj0215@163.com>
2025-02-26 20:29:16 +0800

    2025/2/26

--- Test_list/test_list.cpp
+++ Test_list/test_list.cpp
@@ -1,8 +1,8 @@
 
-//设计一个圆类，求圆的周长和面积
-//圆的周长：2*PI*半径
-//圆的面积：PI*半径*半径
-//PI取3.14
+//���һ��Բ�࣬��Բ���ܳ������
+//Բ���ܳ���2*PI*�뾶
+//Բ�������PI*�뾶*�뾶
+//PIȡ3.14
 
 // #include<iostream>
 // using namespace std;
@@ -13,22 +13,22 @@
 //     private:
 //         double m_r;
 //     public:
-//         //设置半径
+//         //���ð뾶
 //         void setR(double r)
 //         {
 //             m_r=r;
 //         }
-//         //获取半径
+//         //��ȡ�뾶
 //         double getR()
 //         {
 //             return m_r;
 //         }
-//         //获取周长
+//         //��ȡ�ܳ�
 //         double calculateZC()
 //         {
 //             return 2*PI*m_r;
 //         }
-//         //获取面积
+//         //��ȡ���
 //         double calculateMJ()
 //         {
 //             return PI*m_r*m_r;
@@ -38,14 +38,14 @@
 // int main()
 // {
 //     Circle C1;
-//     //设置半径
+//     //���ð뾶
 //     C1.setR(1);
-//     //获取周长
+//     //��ȡ�ܳ�
 //     double ZC=C1.calculateZC();
-//     cout<<"圆的周长为："<<ZC<<endl;
-//     //获取面积
+//     cout<<"Բ���ܳ�Ϊ��"<<ZC<<endl;
+//     //��ȡ���
 //     double MJ=C1.calculateMJ();
-//     cout<<"圆的面积为："<<MJ<<endl;
+//     cout<<"Բ�����Ϊ��"<<MJ<<endl;
     
     
 //     system("pause");
@@ -58,12 +58,12 @@
 // int main()
 // {
 //     cout<<"hello world!"<<endl;
-//     cout<<"调试控制台程序"<<endl;
+//     cout<<"���Կ���̨����"<<endl;
 //     system("pause");
 //     return 0;
 // }
 
-/*类继承示例*/
+/*��̳�ʾ��*/
 // #include<iostream>
 // using namespace std;
 
@@ -72,67 +72,67 @@
 //     public:
 //         void hearder()
 //         {
-//             cout<<"这是一个公共的hearder"<<endl;
+//             cout<<"����һ��������hearder"<<endl;
 //         }
 
 //         void footer()
 //         {
-//             cout<<"这是一个公共的footer"<<endl;
+//             cout<<"����һ��������footer"<<endl;
 //         }
 
 //         void left()
 //         {
-//             cout<<"这是一个公共的left"<<endl;
+//             cout<<"����һ��������left"<<endl;
 //         }
 // };
 
-// //java课程内容
+// //java�γ�����
 // class java:public common
 // {
 //     public:
 //         void java_content()
 //         {
-//             cout<<"java课程内容"<<endl;
+//             cout<<"java�γ�����"<<endl;
 //         }
 // };
 
-// //python课程内容
+// //python�γ�����
 // class python:public common
 // {
 //     public:
 //         void python_content()
 //         {
-//             cout<<"python课程内容"<<endl;
+//             cout<<"python�γ�����"<<endl;
 //         }
 // };
-// //cpp课程内容
+// //cpp�γ�����
 // class cpp:public common
 // {
 //     public:
 //         void cpp_content()
 //         {
-//             cout<<"cpp课程内容"<<endl;
+//             cout<<"cpp�γ�����"<<endl;
 //         }
 // };
 
 
 // void test01()
 // {
-// //java课程内容
+// //java�γ�����
 //     java j;
 //     j.hearder();
 //     j.footer();
 //     j.left();
 //     j.java_content();
 //     cout<<"-------------------"<<endl;
-// //python课程内容
+// //python�γ�����
 //     python p;
 //     p.hearder();
 //     p.footer();
 //     p.left();
 //     p.python_content();
 //     cout<<"-------------------"<<endl;
-// //cpp课程内容
+// //cpp�γ�����
 //     cpp c;
 //     c.hearder();
 //     c.footer();
@@ -148,7 +148,7 @@
 //     return 0;
 // }
 
-/*类继承方式--public、private、protected方式*/
+/*��̳з�ʽ--public��private��protected��ʽ*/
 // #include<iostream>
 // using namespace std;
 
@@ -162,70 +162,70 @@
 //     int m_C;
 // };
 
-// //公共继承
+// //�����̳�
 // class Son1:public Base
 // {
 //     public:
 //         void func()
 //         {
-//             m_A=10;//类内可以访问公共权限内容
-//             m_B=10;//类内可以访问保护权限内容
-//             //m_c=10;//类内不可以访问私有权限内容
+//             m_A=10;//���ڿ��Է��ʹ���Ȩ������
+//             m_B=10;//���ڿ��Է��ʱ���Ȩ������
+//             //m_c=10;//���ڲ����Է���˽��Ȩ������
 //         }
 // };
 // void test01()
 // {
 //     Son1 s1;
-//     s1.m_A=100;//类外可以访问公共权限内容
-//     //s1.m_B=100;//类外不可以访问保护权限内容
-//     //s1.m_C=100;//类外不可以访问私有权限内容
+//     s1.m_A=100;//������Է��ʹ���Ȩ������
+//     //s1.m_B=100;//���ⲻ���Է��ʱ���Ȩ������
+//     //s1.m_C=100;//���ⲻ���Է���˽��Ȩ������
 // }
 
-// //保护继承
+// //�����̳�
 // class Son2:protected Base
 // {
 //     public:
 //         void func()
 //         {
-//             m_A=10;//类内可以访问公共权限内容
-//             m_B=10;//类内可以访问保护权限内容
-//             //m_c=10;//类内不可以访问私有权限内容
+//             m_A=10;//���ڿ��Է��ʹ���Ȩ������
+//             m_B=10;//���ڿ��Է��ʱ���Ȩ������
+//             //m_c=10;//���ڲ����Է���˽��Ȩ������
 //         }
 // };
 // void test02()
 // {
 //     Son2 s2;
-//     //s2.m_A=100;//类外不可以访问公共权限内容
-//     //s2.m_B=100;//类外不可以访问保护权限内容
-//     //s2.m_C=100;//类外不可以访问私有权限内容
+//     //s2.m_A=100;//���ⲻ���Է��ʹ���Ȩ������
+//     //s2.m_B=100;//���ⲻ���Է��ʱ���Ȩ������
+//     //s2.m_C=100;//���ⲻ���Է���˽��Ȩ������
 // }
 
-// //私有继承
+// //˽�м̳�
 // class Son3:private Base
 // {
 //     public:
 //         void func()
 //         {
-//             m_A=10;//类内可以访问公共权限内容
-//             m_B=10;//类内可以访问保护权限内容
-//             //m_c=10;//类内不可以访问私有权限内容
+//             m_A=10;//���ڿ��Է��ʹ���Ȩ������
+//             m_B=10;//���ڿ��Է��ʱ���Ȩ������
+//             //m_c=10;//���ڲ����Է���˽��Ȩ������
 //         }
 // };
 // void test03()
 // {
 //     Son3 s3;
-//     // s3.m_A=100;//类外不可以访问公共权限内容
-//     // s3.m_B=100;//类外不可以访问保护权限内容
-//     // s3.m_C=100;//类外不可以访问私有权限内容
+//     // s3.m_A=100;//���ⲻ���Է��ʹ���Ȩ������
+//     // s3.m_B=100;//���ⲻ���Է��ʱ���Ȩ������
+//     // s3.m_C=100;//���ⲻ���Է���˽��Ȩ������
 // }
 // class Grandson1:public Son3
 // {
 //     public:
 //         void func()
 //         {
-//             // m_A=10;//类内不可以访问公共权限内容
-//             // m_B=10;//类内不可以访问保护权限内容
-//             //m_c=10;//类内不可以访问私有权限内容
+//             // m_A=10;//���ڲ����Է��ʹ���Ȩ������
+//             // m_B=10;//���ڲ����Է��ʱ���Ȩ������
+//             //m_c=10;//���ڲ����Է���˽��Ȩ������
 //         }
 
 // };
@@ -237,7 +237,7 @@
 // }
 
 
-/*继承中的对象模型*/
+/*�̳��еĶ���ģ��*/
 // #include<iostream>
 // using namespace std;
 
@@ -257,7 +257,7 @@
 // };
 // void test01()
 // {
-//     //父类中的私有成员也被子类继承下去了，只是由编译器隐藏后访问不到。
+//     //�����е�˽�г�ԱҲ������̳���ȥ�ˣ�ֻ���ɱ��������غ���ʲ�����
 //     cout<<"size of Son is: "<<sizeof(Son)<<endl;
 // }
 // int main()
@@ -268,40 +268,52 @@
 //     return 0;
 // }
 
-/*继承中的构造和析构顺序*/
-//先构造父类，在构造子类，析构的顺序与构造的顺序相反
+/*�̳��еĹ��������˳��*/
+//�ȹ��츸�࣬�ڹ������࣬������˳���빹���˳���෴
+// #include<iostream>
+// using namespace std;
+// class Base
+// {
+//     public:
+//         Base()
+//         {
+//             cout<<"Base�Ĺ��캯��"<<endl;
+//         }
+//         ~Base()
+//         {
+//             cout<<"Base����������"<<endl;
+//         }
+// };
+// class Son:public Base
+// {
+//     public:
+//         Son()
+//         {
+//             cout<<"Son�Ĺ��캯��"<<endl;
+//         }
+//         ~Son()
+//         {
+//             cout<<"Son����������"<<endl;
+//         }
+// };
+
+// int main()
+// {   
+//     //Base b;
+//     Son s;
+
+//     system("pause");
+//     return 0;
+// }
+
+
 #include<iostream>
 using namespace std;
-class Base
-{
-    public:
-        Base()
-        {
-            cout<<"Base的构造函数"<<endl;
-        }
-        ~Base()
-        {
-            cout<<"Base的析构函数"<<endl;
-        }
-};
-class Son:public Base
-{
-    public:
-        Son()
-        {
-            cout<<"Son的构造函数"<<endl;
-        }
-        ~Son()
-        {
-            cout<<"Son的析构函数"<<endl;
-        }
-};
 
 int main()
-{   
-    //Base b;
-    Son s;
-
+{
+    cout<<"hello world!"<<endl;
+    cout<<"���Կ���̨����"<<endl;
     system("pause");
     return 0;
 }
