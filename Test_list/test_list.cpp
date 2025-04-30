986f6e5c62cec667a5f22b9f58feccf8f93058ed
xushujie <Xufj0215@163.com>
2025-04-30 21:29:25 +0800

    2025/4/30--21：30

--- Test_list/test_list.cpp
+++ Test_list/test_list.cpp
@@ -2932,3 +2932,93 @@
 //     system("pause");
 //     return 0;
 // }
+
+
+// /*deque构造函数*/
+// /*函数原型：
+//     deque<T> deqT;//默认构造函数
+//     deque(beg,end);构造函数将[beg,end)范围内的元素拷贝给本身
+//     deque(n,elem);//构造函数将n个elem拷贝给本身
+//     deque(const deque &deq);//拷贝构造函数
+// */
+// #include<iostream>
+// #include<deque>
+// using namespace std;
+// void printDeque(const deque<int> &d)
+// {
+//     for(deque<int>::const_iterator it=d.begin();it!=d.end();it++)
+//     {
+//         cout<<*it<<" ";
+//     }
+//     cout<<endl;
+// }
+// void test01()
+// {
+//     deque<int> d1;//默认构造函数
+//     for(int i=0;i<10;i++)
+//     {
+//         d1.push_back(i);//尾插法
+//     }
+//     printDeque(d1);
+//     //构造函数将[beg,end)范围内的元素拷贝给本身
+//     deque<int> d2(d1.begin(),d1.end());
+//     printDeque(d2);
+//     //构造函数将n个elem拷贝给本身
+//     deque<int> d3(10,100);
+//     printDeque(d3);
+//     //拷贝构造函数
+//     deque<int> d4(d3);
+//     printDeque(d4);
+// }
+// int main()
+// {
+//     test01();//deque构造函数
+
+//     system("pause");
+//     return 0;
+// }
+
+
+// /*deque容器赋值操作*/
+// /*函数原型：
+//     deque &operator=(const deque &deq);//重载等号操作符
+//     assign(n,elem);//将n个elem拷贝给本身
+//     assign(beg,end);//将迭代器范围内的元素复制到deque容器中
+// */
+// #include<iostream>
+// #include<deque>
+// using namespace std;
+// void printDeque(const deque<int> &d)
+// {
+//     for(deque<int>::const_iterator it=d.begin();it!=d.end();it++)
+//     {
+//         cout<<*it<<" ";
+//     }
+//     cout<<endl;
+// }
+// void test01()
+// {
+//     deque<int> d1;
+//     for(int i=0;i<10;i++)
+//     {
+//         d1.push_back(i);//尾插法
+//     }
+//     printDeque(d1);
+//     deque<int> d2=d1;//重载等号操作符
+//     printDeque(d2);
+//     //将迭代器范围内的元素复制到deque容器中
+//     deque<int> d3;
+//     d3.assign(d1.begin(),d1.begin()+5);//指定区间左闭右开
+//     printDeque(d3);
+//     //将n个elem拷贝给本身
+//     deque<int> d4;
+//     d4.assign(10,100);
+//     printDeque(d4);
+// }
+// int main()
+// {
+//     test01();//deque容器赋值操作
+
+//     system("pause");
+//     return 0;
+// }
