6de6a4794f9a6969730b228095aab6179b43b6a8
xushujie <Xufj0215@163.com>
2025-05-02 20:01:39 +0800

    2025/5/2--20:00

--- test_list.cpp
+++ test_list.cpp
@@ -3318,3 +3318,848 @@
 //     system("pause");
 //     return 0;
 // }
+
+
+// /*stack的基本概念及常用接口*/
+// /*基本概念： 
+//     1、stack是一种先进后出的数据结构，只有一个出口，存入数据称为入栈，取出数据称为出栈
+//     2、栈只有顶端的元素才能被外界使用，因此栈不允许有遍历行为
+//     3、栈可以判断是否为空，可以返回元素个数*/
+// /*常用接口：
+//     1、构造函数：stack(T) stk;//默认构造形式
+//                 stack(const stack &stk)//拷贝构造函数
+//     2、赋值操作：stack& operator=(const stack &stk)//重载赋值操作符
+//     3、数据存取：push(elem)//向栈顶添加数据元素
+//                 pop()//从栈顶移出第一个数据元素
+//                 top()//返回栈顶元素
+//     4、大小操作：empty()//判断堆栈是否为空
+//                 size()//返回栈的大小*/
+// #include<iostream>
+// #include<stack>
+// using namespace std;
+// void test01()
+// {
+//     stack<int> stk1;
+//     stk1.push(10);
+//     stk1.push(20);
+//     stk1.push(30);
+//     stk1.push(40);
+//     stack<int> stk2=stk1;
+//     cout<<"栈stk2的原始大小为:"<<stk2.size()<<endl;
+//     while(!stk2.empty())
+//     {
+//         //查看栈顶数据元素
+//         cout<<"栈顶元素为:"<<stk2.top()<<endl;
+//         //移出栈顶元素
+//         stk2.pop();
+//     }
+//     cout<<"栈stk2的最终大小为:"<<stk2.size()<<endl;
+// }
+// int main()
+// {
+//     test01();
+
+//     system("pause");
+//     return 0;
+// }
+
+
+// /*queue基本概念及常用接口*/
+// /*基本概念：
+//     1、queue是一种先进先出的数据结构，它有两个出口
+//     2、队列容器允许从一端新增数据元素，从另一端移出数据元素
+//     3、队列只有队头和队尾才可以被外界使用，因此队列不允许有遍历行为
+//     4、队列中进入数据称为入队，出数据称为出队*/
+// /*常用接口：
+//     1、构造函数：queue(T) que;//默认构造形式
+//                 queue(const queue &que)//拷贝构造函数
+//     2、赋值操作：queue& operator=(const queue &que)//重载赋值操作符
+//     3、数据存取：push(elem)//往队尾添加元素
+//                 pop()//从队头移出第一个数据元素
+//                 front()//返回第一个元素
+//                 back()//返回最后一个元素
+//     4、大小操作：empty()//判断队列是否为空
+//                 size()//返回队列的大小*/
+// #include<iostream>
+// #include<queue>
+// using namespace std;
+// void test01()
+// {
+//     queue<int> que1;
+//     que1.push(10);
+//     que1.push(20);
+//     que1.push(30);
+//     que1.push(40);
+//     que1.push(50);
+//     que1.push(60);
+//     que1.push(70);
+//     que1.push(80);
+//     que1.push(90);
+//     queue<int> que2=que1;
+//     cout<<"队列的原始大小为:"<<que2.size()<<endl;
+//     //查看队列最后一个元素
+//     cout<<"队列最后一个数据元素为:"<<que2.back()<<endl;
+//     while(!que2.empty())
+//     {
+//         //查看队头数据元素
+//         cout<<"队头数据元素为:"<<que2.front()<<endl;
+//         cout<<"移出队头数据元素:"<<que2.front()<<endl;
+//         //移出队头数据元素
+//         que2.pop();
+//     }
+//     cout<<"队列的最终大小为:"<<que2.size()<<endl;
+// }
+// int main()
+// {
+//     test01();
+
+//     system("pause");
+//     return 0;
+// }
+
+
+// /*list的基本概念:
+//     1、链表是一种物理存储单元上非连续的存储结构，数据元素的逻辑顺序是通过链表中的指针链接实现的
+//     2、链表由一系列结点组成，结点由存储数据元素的数据域和存储下一个结点地址的指针域组成
+//     3、STL中的链表是一个双向循环链表，由于链表的存储方式并不是连续的存储空间，因此链表list中的迭代器只支持前移和后移，属于双向迭代器
+//     4、list的优点：采用动态存储分配，不会造成内存的浪费和溢出
+//                 链表执行插入和删除操作十分方便，修改指针即可，不需要移动大量元素
+//     5、list的缺点：链表灵活，但是空间（指针域）和时间（遍历）额外耗费较大*/
+// /*list构造函数原型：
+//     list<T> lst;//默认构造函数
+//     list(beg,end);//将[beg,end)区间的数据元素拷贝给容器本身
+//     lsit(n,elem);//将n个数据元素elem拷贝给容器本身
+//     list(const list &lst)//拷贝构造函数*/
+// #include<iostream>
+// #include<list>
+// using namespace std;
+// void printList(const list<int> &L)
+// {
+//     for(list<int>::const_iterator it=L.begin();it!=L.end();it++)
+//     {
+//         cout<<*it<<" ";
+//     }
+//     cout<<endl;
+// }
+// void test01()
+// {
+//     list<int> lst1;//默认构造函数
+//     lst1.push_back(10);
+//     lst1.push_back(20);
+//     lst1.push_back(30);
+//     lst1.push_back(40);
+//     lst1.push_back(50);
+//     printList(lst1);
+//     //将[beg,end)区间的数据元素拷贝给容器本身
+//     list<int> lst2(lst1.begin(),lst1.end());
+//     printList(lst2);
+//     //拷贝构造函数
+//     list<int> lst3(lst1);
+//     printList(lst3);
+//     //将n个数据元素elem拷贝给容器本身
+//     list<int> lst4(5,100);
+//     printList(lst4);
+// }
+// int main()
+// {
+//     test01();
+
+//     system("pause");
+//     return 0;
+// }
+
+
+// /*list容器赋值和交换*/
+// /*函数原型：
+//     assign(beg,end);//将[beg,end)区间的数据元素赋值给容器本身
+//     assign(n,elem);//将n个elem拷贝赋值给容器本身
+//     list &operator=(const list &L);//重载等号操作符
+//     swap(lst);//将lst与本身的数据元素进行交换
+// */
+// #include<iostream>
+// #include<list>
+// using namespace std;
+// void printList(const list<int> &L)
+// {
+//     for(list<int>::const_iterator it=L.begin();it!=L.end();it++)
+//     {
+//         cout<<*it<<" ";
+//     }
+//     cout<<endl;
+// }
+// void test01()
+// {
+//     list<int> lst1;//默认构造函数
+//     lst1.push_back(10);
+//     lst1.push_back(20);
+//     lst1.push_back(30);
+//     lst1.push_back(40);
+//     lst1.push_back(50);
+//     printList(lst1);
+//     //将[beg,end)区间的数据元素赋值给容器本身
+//     list<int> lst2;
+//     lst2.assign(lst1.begin(),lst1.end());
+//     printList(lst2);
+//     //将n个elem拷贝赋值给容器本身
+//     list<int> lst3;
+//     lst3.assign(5,100);
+//     printList(lst3);
+//     //重载等号操作符
+//     list<int> lst4=lst3;
+//     printList(lst4);
+//     //将lst与本身的数据元素进行交换
+//     lst4.swap(lst1);
+//     cout<<"与lst1交换后的lst4为:";
+//     printList(lst4);
+//     cout<<"与lst4交换后的lst1为:";
+//     printList(lst1);
+// }
+// int main()
+// {
+//     test01();
+
+//     system("pause");
+//     return 0;
+// }
+
+
+// /*list容器大小操作*/
+// /*函数原型：
+//     size();//返回容器中元素的个数
+//     empty();//判断容器是否为空
+//     resize(int num);//重新指定容器的长度为num，若容器变长则以默认值填充新位置，若变短则末尾超出容器长度的元素被删除
+//     resize(int num,elem);//重新指定容器的长度为num，若容器变长则以elem值填充新位置，若变短则末尾超出容器长度的元素被删除
+// */
+// #include<iostream>
+// #include<list>
+// using namespace std;
+// void printList(const list<int> &L)
+// {
+//     for(list<int>::const_iterator it=L.begin();it!=L.end();it++)
+//     {
+//         cout<<*it<<" ";
+//     }
+//     cout<<endl;
+// }
+// void test01()
+// {
+//     list<int> lst1;//默认构造函数
+//     lst1.push_back(10);
+//     lst1.push_back(20);
+//     lst1.push_back(30);
+//     lst1.push_back(40);
+//     lst1.push_back(50);
+//     printList(lst1);
+//     if(lst1.empty())
+//     {
+//         cout<<"lst1为空!"<<endl;
+//     }
+//     else
+//     {
+//         //返回容器中元素的个数
+//         cout<<"lst1容器中元素的个数为:"<<lst1.size()<<endl;
+//         //重新指定容器的长度为4，删除多余元素
+//         lst1.resize(4);
+//         printList(lst1);
+//         //重新指定容器的长度为10，增加到位置以100填充
+//         lst1.resize(10,100);
+//         printList(lst1);
+//     }
+// }
+// int main()
+// {
+//     test01();
+
+//     system("pause");
+//     return 0;
+// }
+
+
+// /*list容器插入和删除操作*/
+// /*函数原型：
+//     push_back(elem);//在容器尾部加入一个元素
+//     pop_back();//删除容器中最后一个数据元素
+//     push_front(elem);//在容器头部加入一个数据元素
+//     pop_front();//在容器头部删除一个数据元素
+//     insert(pos,elem);//在容器指定位置加入一个数据元素，返回新数据的位置
+//     insert(pos,n,elem);//在容器指定位置加入n个数据元素，无返回值
+//     insert(pos,beg,end);//在容器指定位置加入区间[beg,end)的数据元素，无返回值
+//     clear();//移出容器中所有数据元素
+//     erase(beg,end);//移出区间[beg,end)中的数据元素，返回下一个数据的位置
+//     erase(pos);//删除指定位置的数据，返回下一个数据的位置
+//     remove(elem);//删除容器中所有与elem匹配的数据元素
+// */
+// #include<iostream>
+// #include<list>
+// using namespace std;
+// void printList(const list<int> &L)
+// {
+//     for(list<int>::const_iterator it=L.begin();it!=L.end();it++)
+//     {
+//         cout<<*it<<" ";
+//     }
+//     cout<<endl;
+// }
+// void test01()
+// {
+//     list<int> L1;
+//     L1.push_back(10);
+//     L1.push_back(20);
+//     L1.push_back(30);
+//     L1.push_back(40);
+//     L1.push_back(50);
+//     printList(L1);
+//     //删除容器中最后一个数据元素
+//     L1.pop_back();
+//     printList(L1);
+//     //在容器头部加入一个数据元素
+//     L1.push_front(100);
+//     printList(L1);
+//     //在容器头部删除一个数据元素
+//     L1.pop_front();
+//     printList(L1);
+//     //在容器指定位置加入一个数据元素，返回新数据的位置
+//     L1.insert(L1.end(),50);
+//     printList(L1);
+//     //在容器指定位置加入n个数据元素，无返回值    
+//     L1.insert(L1.begin(),2,300);
+//     printList(L1);
+//     //在容器指定位置加入区间[beg,end)的数据元素，无返回值
+//     //list<int>::iterator it=L1.begin();
+//     L1.insert(L1.end(),L1.begin(),++L1.begin());
+//     //L1.insert(it,it+2,it+7);
+//     printList(L1);
+//     //移出区间[beg,end)中的数据元素，返回下一个数据的位置
+//     L1.erase(L1.begin(),++L1.begin());
+//     printList(L1);
+//     //删除指定位置的数据，返回下一个数据的位置
+//     L1.erase(L1.begin());
+//     printList(L1);
+//     //删除容器中所有与elem匹配的数据元素
+//     L1.remove(300);
+//     printList(L1);
+//     //移出容器中所有数据元素
+//     L1.clear();
+//     cout<<"list容器的大小为:"<<L1.size()<<endl;
+// }
+// int main()
+// {
+//     test01();
+
+//     system("pause");
+//     return 0;
+// }
+
+
+// /*list数据存取*/
+// /*函数原型：
+//     front();//返回第一个元素
+//     back();//返回最后一个元素*/
+// #include<iostream>
+// #include<list>
+// using namespace std;
+// void printList(const list<int> &L)
+// {
+//     for(list<int>::const_iterator it=L.begin();it!=L.end();it++)
+//     {
+//         cout<<*it<<" ";
+//     }
+//     cout<<endl;
+// }
+// void test01()
+// {
+//     list<int> L1;
+//     L1.push_back(10);
+//     L1.push_back(20);
+//     L1.push_back(30);
+//     L1.push_back(40);
+//     L1.push_back(50);
+//     printList(L1);
+//     cout<<"list容器中第一个数据元素为: "<<L1.front()<<endl;
+//     cout<<"list容器中最后一个数据元素为: "<<L1.back()<<endl;
+// }
+// int main()
+// {
+//     test01();
+
+//     system("pause");
+//     return 0;
+// }
+
+
+// /*list反转和排序函数原型：
+//     reverse();//反转链表
+//     sort();//链表排序，默认按升序排列
+// */
+// #include<iostream>
+// #include<list>
+// using namespace std;
+// void printList(const list<int> &L)
+// {
+//     for(list<int>::const_iterator it=L.begin();it!=L.end();it++)
+//     {
+//         cout<<*it<<" ";
+//     }
+//     cout<<endl;
+// }
+// void test01()
+// {
+//     list<int> L1;
+//     L1.push_back(10);
+//     L1.push_back(20);
+//     L1.push_back(30);
+//     L1.push_back(40);
+//     L1.push_back(50);
+//     printList(L1);
+//     //反转链表
+//     L1.reverse();
+//     printList(L1);
+//     //链表排序，默认按升序排列
+//     L1.sort();
+//     printList(L1);
+// }
+// int main()
+// {
+//     test01();
+
+//     system("pause");
+//     return 0;
+// }
+
+
+// /*list排序案例--首先按年龄进行升序排列，当年龄相同时按降序排列*/
+// #include<iostream>
+// #include<list>
+// #include<string>
+// using namespace std;
+// class Person
+// {
+//     public:
+//         Person(string name,int age,int height)
+//         {
+//             this->m_Name=name;
+//             this->m_Age=age;
+//             this->m_Height=height;
+//         }
+//         string m_Name;
+//         int m_Age;
+//         int m_Height;
+// };
+// void printList(const list<Person> &L)
+// {
+//     for(list<Person>::const_iterator it=L.begin();it!=L.end();it++)
+//     {
+//         cout<<"姓名："<<(*it).m_Name<<"     年龄："<<(*it).m_Age<<"     身高："<<(*it).m_Height<<endl;
+//     }
+// }
+// bool compare(Person p1,Person p2)
+// {
+//     if(p1.m_Age==p2.m_Age)
+//     {
+//         return p1.m_Height>p2.m_Height;
+//     }
+//     return p1.m_Age<p2.m_Age;
+// }
+// void test01()
+// {
+//     list<Person> L;
+//     //创建对象
+//     Person p1("刘备",40,175);
+//     Person p2("曹操",40,180);
+//     Person p3("孙权",40,170);
+//     Person p4("张飞",35,170);
+//     Person p5("关羽",36,200);
+//     //插入数据
+//     L.push_back(p1);
+//     L.push_back(p2);
+//     L.push_back(p3);
+//     L.push_back(p4);
+//     L.push_back(p5);
+//     //排序前数据内容
+//     cout<<"排序前："<<endl;
+//     printList(L);
+//     //排序
+//     cout<<"排序后："<<endl;
+//     L.sort(compare);
+//     printList(L);
+// }
+// int main()
+// {
+//     test01();
+
+//     system("pause");
+//     return 0;
+// }
+
+
+// /*set/multiset容器基本概念：
+//     1、所有元素都会在插入时被自动排序
+//     2、set/multiset属于关联式容器，底层结构是用二叉树实现的
+//     3、set不允许容器中有重复的元素，multiset允许容器中有重复的元素*/
+// /*set构造和赋值
+//     构造函数：set<T> st;//默认构造函数
+//              set(cost set &se);//拷贝构造函数
+//     赋值函数：set& operator=(const set &st);//重载等号操作符*/
+// #include<iostream>
+// #include<set>
+// using namespace std;
+// void printSet(const set<int> &S)
+// {
+//     for(set<int>::const_iterator it=S.begin();it!=S.end();it++)
+//     {
+//         cout<<*it<<" ";
+//     }
+//     cout<<endl;
+// }
+// void test01()
+// {
+//     set<int> S1;
+//     S1.insert(10);//插入数据
+//     S1.insert(30);
+//     S1.insert(30);//set不允许插入相同的数据，同一数据只插入一个
+//     S1.insert(50);
+//     S1.insert(40);
+//     S1.insert(20);
+//     printSet(S1);
+//     //拷贝构造函数
+//     set<int> S2(S1);
+//     printSet(S2);
+//     //重载等号操作符
+//     set<int> S3=S1;
+//     printSet(S3);
+// }
+// int main()
+// {
+//     test01();
+
+//     system("pause");
+//     return 0;
+// }
+
+
+// /*set大小和交换*/
+// /*函数原型：
+//     size();//返回容器中元素的数目
+//     empty();//判断容器是否为空
+//     swap(st);//交换两个集合容器*/
+// #include<iostream>
+// #include<set>
+// using namespace std;
+// void printSet(const set<int> &S)
+// {
+//     for(set<int>::const_iterator it=S.begin();it!=S.end();it++)
+//     {
+//         cout<<*it<<" ";
+//     }
+//     cout<<endl;
+// }
+// void test01()
+// {
+//     set<int> S1;
+//     S1.insert(10);//插入数据
+//     S1.insert(30);
+//     S1.insert(50);
+//     S1.insert(40);
+//     S1.insert(20);
+//     printSet(S1);
+//     cout<<"S1容器中的元素个数为:"<<S1.size()<<endl;//查看容器中元素的数目
+//     if(S1.empty())
+//     {
+//         cout<<"S1容器为空!"<<endl;
+//     }
+//     else
+//     {
+//         set<int> S2;
+//         S2.insert(100);//插入数据
+//         S2.insert(300);
+//         S2.insert(500);
+//         S2.insert(400);
+//         S2.insert(200);    
+//         printSet(S2);
+//         cout<<"S2容器中的元素个数为:"<<S2.size()<<endl;//查看容器中元素的数目
+//         S1.swap(S2);
+//         cout<<"交换后的容器S1中的元素为:";
+//         printSet(S1);
+//         cout<<"交换后的容器S2中的元素为:";
+//         printSet(S2);
+//     }
+// }
+// int main()
+// {
+//     test01();
+
+//     system("pause");
+//     return 0;
+// }
+
+
+// /*set容器插入和删除操作*/
+// /*函数原型：
+//     insert(elem);//在容器中插入一个数据元素
+//     clear();//移出容器中所有数据元素
+//     erase(beg,end);//移出区间[beg,end)中的数据元素，返回下一个元素的迭代器
+//     erase(pos);//删除指定位置的数据，返回下一个元素的迭代器
+//     erase(elem);//删除容器中所有与elem匹配的数据元素*/
+// #include<iostream>
+// #include<set>
+// using namespace std;
+// void printSet(const set<int> &S)
+// {
+//     for(set<int>::const_iterator it=S.begin();it!=S.end();it++)
+//     {
+//         cout<<*it<<" ";
+//     }
+//     cout<<endl;
+// }
+// void test01()
+// {
+//     set<int> S1;
+//     S1.insert(10);//插入数据
+//     S1.insert(30);
+//     S1.insert(50);
+//     S1.insert(40);
+//     S1.insert(20);
+//     printSet(S1);
+//     //删除指定位置的数据，返回下一个元素的迭代器
+//     S1.erase(S1.begin());
+//     printSet(S1);
+//     //删除容器中所有与elem匹配的数据元素*/
+//     S1.erase(20);
+//     printSet(S1);
+//     //移出区间[beg,end)中的数据元素，返回下一个元素的迭代器
+//     S1.erase(S1.begin(),++S1.begin());
+//     printSet(S1);
+//     //移出容器中所有数据元素
+//     S1.clear();
+//     cout<<"S1容器中的元素个数为:"<<S1.size()<<endl;
+// }
+// int main()
+// {
+//     test01();
+
+//     system("pause");
+//     return 0;
+// }
+
+
+// /*set查找和统计*/
+// /*函数原型：
+//     find(key);//查找key是否存在，若存在则返回该键的元素的迭代器，若不存在则返回set.end()
+//     count(key);//统计key的元素个数*/
+// #include<iostream>
+// #include<set>
+// using namespace std;
+// void printSet(const set<int> &S)
+// {
+//     for(set<int>::const_iterator it=S.begin();it!=S.end();it++)
+//     {
+//         cout<<*it<<" ";
+//     }
+//     cout<<endl;
+// }
+// void test01()
+// {
+//     set<int> S1;
+//     S1.insert(10);//插入数据
+//     S1.insert(30);
+//     S1.insert(50);
+//     S1.insert(40);
+//     S1.insert(20);
+//     printSet(S1);
+//     //统计key的元素个数
+//     cout<<"容器中元素30的个数为:"<<S1.count(30)<<endl;
+//     //查找key是否存在，若存在则返回该键的元素的迭代器，若不存在则返回set.end()
+//     set<int>::iterator pos=S1.find(30);
+//     if(pos!=S1.end())
+//     {
+//         cout<<"在容器中找到了元素："<<*pos<<endl;
+//     }
+//     else
+//     {
+//         cout<<"在容器中没有找到对应元素"<<endl;
+//     }
+// }
+// int main()
+// {
+//     test01();
+
+//     system("pause");
+//     return 0;
+// }
+
+
+// /*set和multiset的区别：
+//     1、set不允许容器中有重复的元素，multiset允许容器中有重复的元素
+//     2、set插入数据的同时会返回插入结果，表示插入是否成功
+//     3、multiset不会检测数据，因此可以插入重复数据*/
+// #include<iostream>
+// #include<set>
+// using namespace std;
+// void printMultiset(const multiset<int> &S)
+// {
+//     for(multiset<int>::const_iterator it=S.begin();it!=S.end();it++)
+//     {
+//         cout<<*it<<" ";
+//     }
+//     cout<<endl;
+// }
+// void test01()
+// {
+//     set<int> s;
+//     pair<set<int>::iterator,bool> ret=s.insert(10);
+//     if(ret.second)
+//     {
+//         cout<<"第一次插入成功!"<<endl;
+//     }
+//     else
+//     {
+//         cout<<"第一次插入失败！"<<endl;
+//     }
+//     ret=s.insert(10);
+//     if(ret.second)
+//     {
+//         cout<<"第二次插入成功!"<<endl;
+//     }
+//     else
+//     {
+//         cout<<"第二次插入失败！"<<endl;
+//     }
+//     multiset<int> ms;
+//     ms.insert(10);
+//     ms.insert(10);
+//     printMultiset(ms);
+// }
+// int main()
+// {
+//     test01();
+    
+//     system("pause");
+//     return 0;
+// }
+
+
+// /*pair对组（成对出现的数据，利用对组可以返回两个数据）创建方式：
+//     pair<type,type> p(value1,value2);
+//     pair<type,type> p=make_pair(value1,value2);
+// */
+// #include<iostream>
+// #include<string>
+// using namespace std;
+// void test01()
+// {
+//     //第一种创建方式
+//     pair<string,int> p1("张三",18);
+//     cout<<"姓名: "<<p1.first<<"     年龄:"<<p1.second<<endl;
+
+//     //第二种创建方式
+//     pair<string,int> p2=make_pair("李四",20);
+//     cout<<"姓名: "<<p2.first<<"     年龄:"<<p2.second<<endl;
+// }
+// int main()
+// {
+//     test01();
+
+//     system("pause");
+//     return 0;
+// }
+
+
+// /*set容器排序--默认按升序排列，利用仿函数可以改变排序规则*/
+// /*内置数据类型*/
+// #include<iostream>
+// #include<set>
+// using namespace std;
+// void test01()
+// {
+//     class myCompare//仿函数
+//     {
+//         public:
+//             bool operator()(int n1,int n2)
+//             {
+//                 return n1>n2;
+//             } 
+//     };
+//     set<int> S1;
+//     S1.insert(10);//插入数据
+//     S1.insert(30);
+//     S1.insert(50);
+//     S1.insert(40);
+//     S1.insert(20);
+//     for(set<int>::const_iterator it=S1.begin();it!=S1.end();it++)
+//     {
+//         cout<<*it<<" ";
+//     }
+//     cout<<endl;
+//     set<int,myCompare> S2;
+//     S2.insert(10);//插入数据
+//     S2.insert(30);
+//     S2.insert(50);
+//     S2.insert(40);
+//     S2.insert(20);
+//     for(set<int,myCompare>::const_iterator it=S2.begin();it!=S2.end();it++)
+//     {
+//         cout<<*it<<" ";
+//     }
+//     cout<<endl;
+// }
+// int main()
+// {
+//     test01();
+
+//     system("pause");
+//     return 0;
+// }
+
+
+// /*set容器排序--默认按升序排列，利用仿函数可以改变排序规则*/
+// /*自定义数据类型*/
+// #include<iostream>
+// #include<string>
+// #include<set>
+// using namespace std;
+// class Person
+// {
+//     public:
+//         Person(string name,int age)
+//         {
+//             this->m_Name=name;
+//             this->m_Age=age;
+//         }
+//         string m_Name;
+//         int m_Age;
+// };
+// class ComparePerson
+// {
+//     public:
+//         bool operator()(const Person &p1,const Person &p2)
+//         {
+//             //按照年龄进行降序排列
+//             return p1.m_Age>p2.m_Age;
+//         }
+// };
+// void test01()
+// {
+//     set<Person,ComparePerson> S;
+//     //创建Person数据对象
+//     Person p1("刘备",40);
+//     Person p2("张飞",32);
+//     Person p3("赵云",30);
+//     Person p4("关羽",35);
+//     //插入数据
+//     S.insert(p1);
+//     S.insert(p2);
+//     S.insert(p3);
+//     S.insert(p4);
+//     //输出数据
+//     for(set<Person,ComparePerson>::iterator it=S.begin();it!=S.end();it++)
+//     {
+//         cout<<"姓名："<<it->m_Name<<"       年龄："<<it->m_Age<<endl;
+//     }
+// }
+// int main()
+// {
+//     test01();
+
+//     system("pause");
+//     return 0;
+// }
